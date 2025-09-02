7f27f80714cbe7f03f9c2c589a4a658415f333a2
kassiterite <101508245+kassiterite@users.noreply.github.com>
2025-09-02 21:16:11 +0300

    Add files via upload

---
+++ Services/PdfService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace Resume.Services
+{
+    public class PdfService
+    {
+        private readonly IJSRuntime _jsRuntime;
+
+        public PdfService(IJSRuntime jsRuntime)
+        {
+            _jsRuntime = jsRuntime;
+        }
+
+        public async Task GeneratePdfAsync(string elementId = "resume-container", string fileName = "resume.pdf")
+        {
+            await _jsRuntime.InvokeVoidAsync("generatePdf", elementId, fileName);
+        }
+    }
+}
